60a03b0cf322e43869e480167eefd7845b8c5fe7
laileon <leon@igola.com>
2018-05-18 18:16:05 +0800

    network

--- app/src/main/java/clem/app/jnews/MainActivity.kt
+++ app/src/main/java/clem/app/jnews/MainActivity.kt
@@ -44,7 +44,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         async(UI) {
             tryCatch({
                 it.printStackTrace()
-            }) {
+            } , {
                 newsAsync?.cancelByActive()
                 newsAsync = RetrofitHelper.retrofitService.getNewsList("national/list")
                 val result = newsAsync?.await()
@@ -53,7 +53,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                     return@async
                 }
                 toast(result.size.toString())
-            }
+            })
         }
     }
 
